4a17ed009337bab0e413c5984154976338634910
nowshadjaman21 <nowshad.nirob@gmail.com>
2024-10-04 00:21:42 +0600

    PH tube making

---
+++ video.js
@@ -0,0 +1,74 @@
+// show catagories
+const loadCatagories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displaCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+loadCatagories();
+
+const displaCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    console.log(item);
+
+    const button = document.createElement("button");
+    button.classList.add("btn", "btn-ghost");
+    button.innerHTML = item.category;
+    document.getElementById("categories").appendChild(button);
+  });
+};
+
+// show videos
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+loadVideos();
+
+const displayVideos = (videos) => {
+  const videosContainer = document.getElementById("videos-container");
+
+  videos.forEach((video) => {
+    console.log(video);
+
+    const videoDiv = document.createElement("div");
+    videoDiv.classList = "card card-compact ";
+    videoDiv.innerHTML = `
+         <figure class="h-[200px] relative" >
+    <img
+      src=${video.thumbnail}
+      class="w-full h-full object-cover"
+      alt="video" />
+
+    ${video.others.posted_date?.length === 0 ? "" : `<span class="absolute bottom-2 right-2 bg-black text-white p-1 px-2 rounded-lg">${video.others.posted_date}</span>`}
+
+    
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+    <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} class="w-12 h-12 rounded-full" />
+    </div>
+    <div >
+    <h2 class="font-bold">${video.title}</h2>
+    <div class="flex items-center gap-2">
+    <p class="text-gray-400">${video.authors[0].profile_name}</p>
+
+    ${video.authors[0].verified === true ?
+         `<img class="w-5" src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" />` : ``}
+
+    </div>
+   
+    
+    </div>
+
+   
+  </div>
+        
+        `;
+
+    videosContainer.append(videoDiv);
+  });
+};
