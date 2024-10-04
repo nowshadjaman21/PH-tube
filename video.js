9df62a398e96489f84141ebd34bd5704ab50b515
nowshadjaman21 <nowshad.nirob@gmail.com>
2024-10-04 20:20:55 +0600

    PH tube making

--- video.js
+++ video.js
@@ -2,20 +2,22 @@
 const loadCatagories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data) => displaCategories(data.categories))
+    .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error));
 };
 loadCatagories();
 
-const displaCategories = (categories) => {
+const displayCategories = (categories) => {
   const categoriesContainer = document.getElementById("categories");
   categories.forEach((item) => {
-    console.log(item);
 
-    const button = document.createElement("button");
-    button.classList.add("btn", "btn-ghost");
-    button.innerHTML = item.category;
-    document.getElementById("categories").appendChild(button);
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML =
+    `
+    <button id="${item.category_id}" class="btn btn-primary"  onclick="loadcategoriesVideos('${item.category_id}')">${item.category}</button>
+    `
+    
+    categoriesContainer.append(buttonContainer);
   });
 };
 
@@ -30,12 +32,28 @@ loadVideos();
 
 const displayVideos = (videos) => {
   const videosContainer = document.getElementById("videos-container");
+  videosContainer.innerHTML = "";
+
+if(videos.length === 0){
+  videosContainer.classList.remove("grid");
+  const emptyDiv = document.createElement("div");
+  emptyDiv.classList = "flex justify-center items-center";
+  emptyDiv.innerHTML = `
+  <h1 class="text-3xl font-bold text-center">No Videos Found</h1>
+  `
+  videosContainer.append(emptyDiv);
+  return;
+}
+else{
+  videosContainer.classList.add("grid");
+}
 
   videos.forEach((video) => {
     console.log(video);
 
     const videoDiv = document.createElement("div");
     videoDiv.classList = "card card-compact ";
+    
     videoDiv.innerHTML = `
          <figure class="h-[200px] relative" >
     <img
@@ -71,4 +89,12 @@ const displayVideos = (videos) => {
 
     videosContainer.append(videoDiv);
   });
+};
+// show videos based on catagory
+
+const loadcategoriesVideos = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category))
+    .catch((error) => console.log(error));
 };
